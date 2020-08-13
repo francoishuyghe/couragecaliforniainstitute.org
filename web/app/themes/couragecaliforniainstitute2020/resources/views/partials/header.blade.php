532e44acc1275977c5a2c935ccbef259347b9702
Francois Huyghe <francois@13milliseconds.com>
2020-08-13 18:05:14 -0400

    Mobile menu

--- web/app/themes/couragecaliforniainstitute2020/resources/views/partials/header.blade.php
+++ web/app/themes/couragecaliforniainstitute2020/resources/views/partials/header.blade.php
@@ -5,6 +5,7 @@
     </a>
     <a class="hamburger" href="#">
       <i class="far fa-bars"></i>
+      <i class="fal fa-times"></i>
     </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
