48722a6a017157370c928b337c6cbc48f12f40f8
Francois Huyghe <francois@13milliseconds.com>
2021-04-26 16:17:48 -0400

    more design

--- web/app/themes/couragecaliforniainstitute2020/resources/views/partials/blog-content.blade.php
+++ web/app/themes/couragecaliforniainstitute2020/resources/views/partials/blog-content.blade.php
@@ -7,15 +7,9 @@
     <div class="row">
         @foreach ($blog_posts as $post)
         @php setup_postdata($post) @endphp
-        @if($loop->iteration < 4 )
-        <div class="col-md-4">
-            @include('partials.post-block')
+        <div class="post-wrap">
+        @include('partials.post-block')
         </div>
-        @else
-        <div class="col-md-12 horizontal">
-            @include('partials.post-block')
-        </div>
-        @endif
         @php wp_reset_postdata() @endphp
         @endforeach
     </div>
