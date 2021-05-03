ea37934d6b065354565e94c3fdacc9c5086d7332
Francois Huyghe <francois@13milliseconds.com>
2021-05-03 19:53:11 -0400

    Blog categories

--- web/app/themes/couragecaliforniainstitute2020/resources/views/partials/blog-header.blade.php
+++ web/app/themes/couragecaliforniainstitute2020/resources/views/partials/blog-header.blade.php
@@ -2,11 +2,13 @@
   <h1>{{ the_title() }}</h1>
   {!! the_content() !!}
   <div class="cats">
-      @php $cats = get_categories() @endphp
+      @php $cats = get_terms( 'category', array(
+        'parent'    => 65,
+        'hide_empty' => false
+      )); 
+    @endphp
       @foreach ($cats as $category)
-      @if(strpos($category->name, 'County') == false && $category->name !== 'Uncategorized')
-      <a href="{{get_category_link($category->term_id) }}">{{$category->name }}</a>
-      @endif
+        <a href="{{get_category_link($category->term_id) }}" @if($category->count > 0)class="active"@endif>{{$category->name }}</a>
       @endforeach
   </div>
 </div>
