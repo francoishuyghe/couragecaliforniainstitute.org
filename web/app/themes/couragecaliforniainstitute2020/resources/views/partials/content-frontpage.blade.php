94f66b7981485e07e6cb797dd2fe1dac5ba020e1
Francois Huyghe <francois@13milliseconds.com>
2021-09-03 10:27:16 -0400

    finalized top banner

--- web/app/themes/couragecaliforniainstitute2020/resources/views/partials/content-frontpage.blade.php
+++ web/app/themes/couragecaliforniainstitute2020/resources/views/partials/content-frontpage.blade.php
@@ -1,9 +1,12 @@
-<section id="intro" style="background-image: url({{ $data['intro']['image']['sizes']['large'] }})">
+<section id="intro">
     <div class="container">
         <p class="subtitle">{!! $data['intro']['subtitle'] !!}</p>
         <h2>{!! $data['intro']['title'] !!}</h2>
         <p>{{ $data['intro']['text'] }}</p>
         <a class="button" href="{{ $data['intro']['button']['url'] }}">{{ $data['intro']['button']['title'] }}</a>
+        <img class="top" src="{{ $data['intro']['image']['sizes']['large'] }}" />
+        <img class="left" src="{{ $data['intro']['image_left']['sizes']['large'] }}" />
+        <img class="right" src="{{ $data['intro']['image_right']['sizes']['large'] }}" />
     </div>
 </section>
 
